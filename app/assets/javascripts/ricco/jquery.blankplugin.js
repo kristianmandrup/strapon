262626351491e898a19e30c79e53213c6ea1d5ac
matt <mam@crd.dk>
2012-04-29 16:24:37 +0200

    starting point for plugin dev

---
+++ app/assets/javascripts/ricco/jquery.blankplugin.js
@@ -0,0 +1,10 @@
+(function($) {
+  //shell for plugin code
+  $fn.blankplugin = function(){
+      // plugin code
+      return this.each(function() {
+        // Do something to each item
+      });
+  },
+
+})(jQuery);
---
+++ app/assets/javascripts/ricco/jquery.highlight_once.js
@@ -0,0 +1,10 @@
+(function($) {
+  //shell for plugin code
+  $fn.highlightOnce = function(){
+      // plugin code
+      return this.each(function() {
+        // Do something to each item
+      });
+  },
+
+})(jQuery);
